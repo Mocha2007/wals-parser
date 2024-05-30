53063959f9cd4680078314337d742dee11f11325
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-05-30 08:23:14 -0400

    image thing

--- Geo.cs
+++ Geo.cs
@@ -1,80 +1,39 @@
+using System.Drawing;
+
 namespace WalsParser {
-	enum Region {
-		EUROPE,
-		CAUCASUS,
-		SIBERIA,
-		ASIA_CENTRAL,
-		ASIA_EAST,
-		ASIA_SOUTHWEST,
-		INDIA,
-		INDOCHINA,
-		AFRICA_NORTH,
-		AFRICA_SUBSAHARAN,
-		INDONESIA,
-		OCEANIA,
-		AMERICA_NORTH_NORTH,
-		AMERICA_CENTRAL,
-		AMERICA_SOUTH,
-		CARRIBEAN,
+
+	enum Region : uint {
+		EUROPE = 0xFFFFFFFF,
+		CAUCASUS = 0xFF000000,
+		SIBERIA = 0xFF404040,
+		ASIA_CENTRAL = 0xFF808080,
+		ASIA_EAST = 0xFFC0C0C0,
+		ASIA_SOUTHWEST = 0xFFFFFF00,
+		INDIA = 0xFF00FF00,
+		INDOCHINA = 0xFFFFE000,
+		AFRICA_NORTH = 0xFF0000FF,
+		AFRICA_SUBSAHARAN = 0xFF008000,
+		INDONESIA = 0xFF00C080,
+		OCEANIA = 0xFFFFC080,
+		AMERICA_NORTH_NORTH = 0xFFFF0000,
+		AMERICA_CENTRAL = 0xFF00FFFF,
+		AMERICA_SOUTH = 0xFFFF8000,
+		CARRIBEAN = 0xFF80FF00,
 	}
 	static class Geo {
+		const string MAP_FILENAME = "regions.png";
+		static readonly Bitmap map = new Bitmap(MAP_FILENAME);
+		static readonly int height = map.Height;
+		static readonly int width = map.Width;
 		public static Region FromLatLon(double lat, double lon){
-			// Western Hemisphere
-			if (lon < -26){
-				if (lat < 14.4){
-					if (lon > -82)
-						return Region.AMERICA_SOUTH;
-					return Region.OCEANIA;
-				}
-				if (lon > -80 && lat < 25)
-					return Region.CARRIBEAN;
-				if (lon > -115 && lon < -80 && lat < 30)
-					return Region.AMERICA_CENTRAL;
-				if (lat > 40 || lon > -115)
-					return Region.AMERICA_NORTH_NORTH;
-				return Region.OCEANIA;
-			}
-			// Europe + Africa
-			if (lon < 65){
-				if (lat < 12)
-					return Region.AFRICA_SUBSAHARAN;
-				// North Africa + Middle East
-				if (lat < 37){
-					if (lon < 41)
-						return Region.AFRICA_NORTH;
-					return Region.ASIA_SOUTHWEST;
-				}
-				// Europe + Caucasus
-				if (lon < 50){
-					if (lat < 45 && lon > 45)
-						return Region.CAUCASUS;
-					return Region.EUROPE;
-				}
-				if (lat > 55)
-					return Region.ASIA_CENTRAL;
-				return Region.SIBERIA;
-			}
-			// India + other half of Central Asia
-			if (lon < 93){
-				if (lat < 37)
-					return Region.INDIA;
-				if (lat < 55)
-					return Region.ASIA_CENTRAL;
-				return Region.SIBERIA;
-			}
-			// other third of Siberia
-			if (lat > 51)
-				return Region.SIBERIA;
-			// east easia
-			if (lat > 22)
-				return Region.ASIA_EAST;
-			// oceania
-			if (lat < -10 || lon > 130)
-				return Region.OCEANIA;
-			// indonesia
-			if (lat < 6 || lon > 113)
-				return Region.INDONESIA;
-			return Region.INDOCHINA;
+			Tuple<int, int> coords = LatLonToXY(lat, lon);
+			uint answer = (uint)map.GetPixel(coords.Item1, coords.Item2).ToArgb();
+			return (Region)answer;
+		}
+		static Tuple<int, int> LatLonToXY(double lat, double lon){
+			int x = (int)((lon + 180)/360 * width);
+			int y = (int)((90 - lat)/180 * height);
+			return new Tuple<int, int>(x, y);
 		}
 	}
 }
