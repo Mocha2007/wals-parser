7d48f69e14b095f1acd9334c55b7738cdb7ef2b6
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-05-30 19:04:36 -0400

    Amazon

--- Geo.cs
+++ Geo.cs
@@ -20,13 +20,14 @@ namespace WalsParser {
 		AMERICA_NORTH_NORTH = 0xFFFF0000,
 		PLAINS = 0xFFC080FF,
 		AMERICA_CENTRAL_PERIPHERAL = 0xFF00FFFF,
-		AMERICA_SOUTH = 0xFFFF8000,
+		AMERICA_SOUTH_PERIPHERAL = 0xFFFF8000,
 		CASCADIA = 0xFFC0FFC0,
 		CARRIBEAN = 0xFF80FF00,
 		OCEANIA = 0xFFFFC080,
 		NEW_GUINEA = 0xFFFF8080,
 		AUSTRALIA = 0xFFFFC0FF,
 		MESOAMERICA = 0xFF400080,
+		AMAZON = 0xFF804080,
 	}
 	class Region {
 		public static readonly List<Region> regions = new();
@@ -53,7 +54,7 @@ namespace WalsParser {
 		static readonly Region AMERICA_CENTRAL = new("AMERICA_CENTRAL", new Province[]{
 			Province.AMERICA_CENTRAL_PERIPHERAL, Province.MESOAMERICA
 		});
-		// static readonly Region AMERICA_SOUTH = new("AMERICA_SOUTH", new Province[]{Province.AMERICA_SOUTH});
+		static readonly Region AMERICA_SOUTH = new("AMERICA_SOUTH", new Province[]{Province.AMERICA_SOUTH_PERIPHERAL, Province.AMAZON});
 		static readonly Region ASIA = new("ASIA", new Province[]{
 			Province.SIBERIA, Province.CAUCASUS, Province.ASIA_SOUTHWEST,
 			Province.ASIA_CENTRAL, Province.INDIA, Province.ASIA_EAST, Province.INDOCHINA, Province.INDONESIA
